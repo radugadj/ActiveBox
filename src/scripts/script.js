9a8090af4b7ba47bc7fe4145eb5f5ed553449373
radugadj <radugadj@gmail.com>
2024-01-23 01:46:10 +0300

    fix: fixed toggle-menu

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -18,8 +18,8 @@
         window.addEventListener('scroll', handleScroll);
 
         // Получаем ссылки на элементы
-        let menuButton = document.querySelector('.burger-menu');
-        let menu = document.querySelector('.menu');
+        let menuButton = document.querySelector('.header__toggle-menu');
+        let menu = document.querySelector('.nav');
 
         // // Добавляем слушатели событий
         menuButton.addEventListener('click', toggleMenu);
@@ -27,6 +27,7 @@
         // // Функция для открытия/закрытия меню
         function toggleMenu() {
             menu.classList.toggle('open');
+            menuButton.classList.toggle('open');
         }
 
         // // Добавляем слушатель события для каждого пункта меню
